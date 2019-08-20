b5e04dddefef6acb57bbeac9f67e59a040765c77
xenjacob <utterdance@gmail.com>
2019-08-20 19:28:30 -0400

    dyamic infowindow
    Achieved dynamic infowindow content swapping via its setContent function!

--- lines.js
+++ lines.js
@@ -1,31 +1,32 @@
-var testsl;
+let testsl;
 
 // fetch all songlines
 $.ajax({
     url: "songlineslib.json",
     dataType: "json",
     method: "GET"
-}).done(function(data) {
+}).done(function (data) {
     testsl = data[0];
     console.log(testsl);
 
     // map songlines
-    var path1 = new google.maps.Polyline({
+    let path1 = new google.maps.Polyline({
         path: testsl.path,
         strokeColor: '#4d89ea',
         strokeOpacity: 1.0,
         strokeWeight: 8,
     });
-    path1.setMap(map);    
-  
-    // 
-    var songlineinfo1 = new google.maps.InfoWindow({
-        content: document.getElementById("songline"),
+    path1.setMap(map);
+
+    let popupwindow = new google.maps.InfoWindow({
+        content: document.getElementById("popupinfo"),
         position: testsl.path[0]
     });
-    
-    path1.addListener('click', function() { 
-        songlineinfo1.open(map);
+
+    path1.addListener('click', (e) => {
+        popupwindow.setContent("<h2>" + testsl.songlinename + "</h2><br />" +
+                "<a href='/scores/" + testsl.scoreURL + "'>Score</a><br />");
+        popupwindow.open(map);
     });
 
 });
